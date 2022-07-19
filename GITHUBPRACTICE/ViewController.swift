d70c8971542cd7df21a9ce34462a1412061d18f0
Alisha Rajadhyksha <alisharajad@gmail.com>
2022-07-19 15:15:56 -0400

    Add all of homepage stuff

--- GITHUBPRACTICE/ViewController.swift
+++ GITHUBPRACTICE/ViewController.swift
@@ -9,12 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var label: UILabel!
+
+
+    @IBOutlet weak var lillies: UILabel!
+    @IBOutlet weak var smallInfo: UILabel!
+    @IBOutlet weak var imgHair: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
 
+    @IBAction func infoBtn(_ sender: Any) {
+    }
+    
+    
+    @IBAction func tutorialBtn(_ sender: Any) {
+    }
+    
+    
+    @IBAction func communicateBtn(_ sender: Any) {
+    }
 }
 
